aec32d92008c36ad4134759c72cbdac1f3ca9988
razat1 <mir_vizy2016@mail.ru>
2024-02-16 11:38:07 +0300

    second

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ const seconds = document.querySelector('#seconds')
 const currentYear = new Date().getFullYear();
 const nextYear = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+const currentTime = new Date();
+const diff = nextYear - currentTime;
+
+const daysNewYear = Math.floor(diff / 1000 / 60 / 60 / 24)
+const hoursNewYear = Math.floor(diff / 1000 / 60 / 60) % 24
+const minutesNewYear = Math.floor(diff / 1000 / 60) % 60
+const secondsNewYear = Math.floor(diff / 1000) % 60 1
 
